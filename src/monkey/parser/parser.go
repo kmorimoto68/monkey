0cb1fd3a86034e80cfa1f0cdc21c90d4205ef0bf
kmorimoto68 <kmorimoto68@gmail.com>
2020-01-25 22:29:34 +0900

    配列まで

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -17,6 +17,7 @@ const (
 	PRODUCT // *
 	PREFIX // -X or !X
 	CALL // myFunction(X)
+	INDEX // array[index]
 )
 
 var precedences = map[token.TokenType]int {
@@ -29,6 +30,7 @@ var precedences = map[token.TokenType]int {
 	token.SLASH: PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN: CALL,
+	token.LBRACKET: INDEX,
 }
 
 type (
@@ -82,6 +84,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.STRING, p.parseStringLiteral)
 
 	p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
+
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	// 2つのトークンを読み込む。curTokenとpeekTokenの両方がセットされる
 	p.nextToken()
@@ -464,4 +468,16 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 	}
 
 	return list
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+	return exp
 }
