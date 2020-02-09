2814ad6490083f4a40758d8b00065804100d218f
kmorimoto68 <kmorimoto68@gmail.com>
2020-02-09 10:17:45 +0900

    付録まで終了

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -89,6 +89,8 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.registerPrefix(token.LBRACE, p.parseHashLiteral)
 
+	p.registerPrefix(token.MACRO, p.parseMacroLiteral)
+
 	// 2つのトークンを読み込む。curTokenとpeekTokenの両方がセットされる
 	p.nextToken()
 	p.nextToken()
@@ -511,4 +513,22 @@ func (p *Parser) parseHashLiteral() ast.Expression {
 	}
 
 	return hash
+}
+
+func (p *Parser) parseMacroLiteral() ast.Expression {
+	lit := &ast.MacroLiteral{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	lit.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	lit.Body = p.parseBlockStatement()
+
+	return lit
 }
